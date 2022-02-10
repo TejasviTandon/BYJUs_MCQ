2fc36408b3f3cda5c78c8a49d9a31130692f6470
Tejsvi Tandon <tejasvi.tandon@phablecare.com>
2022-02-10 14:31:06 +0530

    Initial Commit

---
+++ MCQs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MCQs
+//
+//  Created by Tejasvi Tandon on 10/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
